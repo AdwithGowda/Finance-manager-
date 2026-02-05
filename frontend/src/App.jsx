81c12bb7dcaa4a4dba65611c955cd46a482cf844
AdwithGowda <126180447+AdwithGowda@users.noreply.github.com>
2026-02-05 18:22:37 +0530

    Update API_URL to use environment variable

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Auth from "./pages/Auth";
 
-const API_URL = 'http://localhost:8000';
+const API_URL = import.meta.env.VITE_API_URL;
 
 /* -------------------- AXIOS AUTH INTERCEPTOR -------------------- */
 axios.interceptors.request.use((config) => {
